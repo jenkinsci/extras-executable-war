014a0bede61f5fcb064e2b7ef2cf32187af80123
kohsuke <kohsuke@@java.net>
2008-08-17 07:26:20 +0000

    need a class in a non-root package for the war file discovery via Which.jarFile

---
+++ src/main/java/executable/Executable.java
@@ -0,0 +1,14 @@
+package executable;
+
+/**
+ * Marker.
+ *
+ * <p>
+ * This is just so that other classes can refer to something
+ * in the executable-war module (classes in the root package
+ * are not referencable)
+ *
+ * @author Kohsuke Kawaguchi
+ */
+public class Executable {
+}
