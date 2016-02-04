7fb8540063d0b21ed4f78629cf9c8b2ed9960021
Stephen Connolly <stephen.alan.connolly@gmail.com>
2016-02-04 14:43:08 +0000

    [JENKINS-32765] Adapt to changed property name
    See https://github.com/stephenc/jenkins/commit/c0cbff72972d7611da187f6258116e6b60d78e7d

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -158,7 +158,7 @@ public class Main {
         }
         for (int i = 0; i < args.length; i++) {
             if (args[i].startsWith("--pluginroot=")) {
-                System.setProperty("hudson.ClassicPluginStrategy.WORK_DIR",
+                System.setProperty("hudson.PluginManager.workDir",
                         new File(args[i].substring("--pluginroot=".length())).getAbsolutePath());
                 // if specified multiple times, the first one wins
                 break;
